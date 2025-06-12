0d33f1eb7eac412b4fd9b3362996984033deed8c
NO.0342 <2215145346@qq.com>
2025-06-12 14:12:24 +0800

    1.整理代码

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "stdio.h"
-<<<<<<< HEAD
 int sub(int a,int b)
 {
     return (a-b);
+}
+int add(int a,int b)
+{
+    return (a+b);
 }
 
 void main()
@@ -10,15 +13,7 @@ void main()
     printf("hello world \r\n");
     printf("sub:%d \r\n",sub(5,3));
     printf("sub_dev_finish \r\n");
-=======
-
-int add(int a,int b)
-{
-    return (a+b);
-}
-void main()
-{
-    printf("hello world:%d \r\n",add(2,3));
+    printf("add:%d \r\n",add(2,3));
     printf("add_dev_finish \r\n");
->>>>>>> 7a34a40bea7ccea5da31e83ab603165b740abcf8
 }
+
